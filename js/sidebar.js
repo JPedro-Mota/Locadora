83f3a4d7d6fb93dafb0663f87da7d49d0334c892
Mota <joaopedromota67@gmail.com>
2024-07-11 13:04:25 -0300

    add table

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,6 +1,6 @@
 document.getElementById('open_btn').addEventListener('click', function(){
     document.getElementById('sidebar').classList.toggle('open-sidebar');
     document.body.classList.toggle('open-sidebar'); 
-    document.querySelector('.user').classList.toggle('open-sidebar');
+    document.querySelector('header').classList.toggle('open-sidebar');
     // adicionado
   })
