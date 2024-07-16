216414807f63b3be276a9ee30eb94c2e03c9e7ca
Mota <joaopedromota67@gmail.com>
2024-07-15 21:53:10 -0300

    atualização

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,5 +2,5 @@ document.getElementById('open_btn').addEventListener('click', function(){
     document.getElementById('sidebar').classList.toggle('open-sidebar');
     document.body.classList.toggle('open-sidebar'); 
     document.querySelector('header').classList.toggle('open-sidebar');
-    // adicionado
+    
   })
